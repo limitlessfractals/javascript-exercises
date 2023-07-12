ced4772e34e8fe788615eb3b3c9eb98e4ca8eaee
Limitless <q@prismatica.cards>
2023-07-12 01:30:32 -0500

    finished reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
-};
-
+const reverseString = function(string) {
+    let rev = "";
+    let i = string.length;
+    while (i > 0) {
+      rev += string[i-1];
+      i -= 1;
+    }
+    return rev;
+  };
+  
 // Do not edit below this line
 module.exports = reverseString;
