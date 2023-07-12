7cfdbff6461d0a565c511577109d6a3afa224723
Limitless <q@prismatica.cards>
2023-07-12 01:50:56 -0500

    finished removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,12 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function(arr, ...elements) {
+    for (let i = arr.length - 1; i >= 0; i--) {
+      if (elements.includes(arr[i])) {
+        arr.splice(i, 1);
+      }
+    }
+    return arr;
+  };
+  
 
 // Do not edit below this line
 module.exports = removeFromArray;
