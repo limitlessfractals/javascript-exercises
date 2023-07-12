4fdb53154af9731789bc6d9746792c329d708f1a
Limitless <q@prismatica.cards>
2023-07-12 02:01:46 -0500

    sumAll passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,16 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if (typeof start !== 'number' || typeof end !== 'number') return "ERROR";
+    if (start < 0 || end < 0) return "ERROR";
+    let sum = 0;
+    if (start > end) {
+        const swap = start;
+        start = end;
+        end = swap;
+    }
+    for (let i = start; i < end + 1; i++) {
+        sum += i;
+    }
+    return sum;
 
 };
 
